23fd249a9d9533d6ccf22c5b2a97d4e79f3487bb
NASUNJIN <sj010618@gmail.com>
2023-09-19 09:58:38 +0900

    Email LogIn

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
 //import React from "react";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Router from "components/Router";
 import { authService } from "fbase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
-  console.log(authService.currentUser);
   // usState에서 유저의 로그인 여부를 확인 할 수 있음
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser); // eslint-disable-line no-unused-vars
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // eslint-disable-line no-unused-vars
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+        const uid = user.uid
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }); 
+
   return (
     <>
-      <Router isLoggedIn={ isLoggedIn } />
+      {init ? <Router isLoggedIn={ isLoggedIn } /> : "Initializing..."}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);  //false : Log In, true : Create newAccount
+    const [error, setError] = useState("");
 
     // const onChange = (event) => {
     //     const {target: {name, value}} = event;
@@ -11,16 +14,40 @@ const Auth = () => {
     //     } else if(name ==="password") {
     //         setPassword(value);
     //     }
-    // };
-    const onSubmit = (event) => {
-        event.preventDefault();
+    // };  onChange={e => setEmail(e.target.value)} 로 사용 가능
+
+    const onSubmit = async (event) => {
+        event.preventDefault();  // 폼 제출 시 페이지 새로고침 방지
+        // 만약 없을 시 로그인하면 링크에 이메일이랑 비번 친게 보임
+        // 리엑트 코드 및 state 초기화
+        try {
+            let data;
+            const auth = getAuth();
+            if (newAccount) {
+                // create account
+                const data = await createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+                    console.log(userCredential.user)
+                });
+            } else {
+                // Login In
+                const data = await signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+                    console.log(userCredential.user)
+                });
+            }
+            console.log(data);
+        } catch(error) {
+            setError(error.message);
+        }
     };
+
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+
     return(
         <div>
             <form onSubmit={onSubmit}>
                 <input 
                     name="email" 
-                    type="text" 
+                    type="email" 
                     placeholder="Email" 
                     required 
                     value={email} 
@@ -38,9 +65,13 @@ const Auth = () => {
                 />
                 <input 
                     type="submit"
-                    value="Log In"
-                 />
+                    value={newAccount ? "Create Account" : "Sign In"}
+                />
+                {error}
             </form>
+            <span onClick={toggleAccount}>
+                {newAccount ? "Sign In" : "Create Account"}
+            </span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
